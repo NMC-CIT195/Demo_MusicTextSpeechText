bb7ab231b7bf33a0812be5749473c9ed9343a0be
John Velis <john@johnvelis.com>
2017-04-05 16:03:50 -0400

    initial submit

---
+++ Demo_MusicTextSpeechText/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MusicTextSpeechText
+{
+    class Program
+    {
+        //static Dictionary<>
+        static void Main(string[] args)
+        {
+            bool running = true;
+
+            PlayMusic();
+
+            while (running)
+            {
+                
+            }
+
+        }
+
+        static void PlayMusic()
+        {
+            using (SoundPlayer scaryMusic = new SoundPlayer(@"Media\scary-suspense.wav"))
+            {
+                scaryMusic.PlaySync();
+            }
+        }
+    }
+}
